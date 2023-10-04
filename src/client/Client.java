2ab46b9996ac10745458e4b3e5922ec694cfdb0e
Chrisvasa <Christophervasankari@gmail.com>
2023-10-04 18:24:07 +0200

    Added Server and Client connection. Added Server.java that will listen to incoming connections or clients, and creating a socket to communicate with them. Added ClientHandler that keeps track of all clients. Added Client.java that allows a user to connect and send/recieve messages.

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,24 +1,96 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) throws InterruptedException {
-        Socket socket = null;
-        int portNumber = 4444;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String username;
+
+    public Client(Socket socket, String username) {
         try {
-            socket = new Socket("localhost", portNumber);
-            Thread.sleep(1000);
-            Thread server = new Thread(new ServerThread(socket, "test"));
-            server.start();
+            this.socket = socket;
+            this.username = username;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (IOException e) {
-            System.err.println("Fatal connection error!");
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            System.err.println("Fatal Connection error!");
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    // Send messages to our clienthandler
+    public void sendMessage() {
+        try {
+            bufferedWriter.write(username);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            Scanner scanner = new Scanner(System.in);
+            while (socket.isConnected()) {
+                String messageToSend = scanner.nextLine();
+                bufferedWriter.write(username + ": " + messageToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    // This will be waiting for a message that are
+    // broadcasted broadcastMessage in ClientHandler
+    // Each client will have a separate thread that is waiting for messages.
+    public void listenForMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String messageFromGroupChat;
+
+                while (socket.isConnected()) {
+                    try {
+                        messageFromGroupChat = bufferedReader.readLine();
+                        System.out.println(messageFromGroupChat);
+                    } catch (IOException e) {
+                        closeEverything(socket, bufferedReader, bufferedWriter);
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter your username for the group chat: ");
+        String username = scanner.nextLine();
+        Socket socket = new Socket("localhost", 6969);
+        Client client = new Client(socket, username);
+        // These are both blocking methods (Basically halt our the client)
+        // But since these are on separate threads
+        // they both will be running at the same time
+        client.listenForMessage();
+        client.sendMessage();
     }
 }
---
+++ src/server/ClientHandler.java
@@ -0,0 +1,94 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+
+    // List to keep track of all our clients. If a client sends a message,
+    // we can loop through this and send each client the message
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    // Passed from server class - Establish connection between client and server
+    private Socket socket;
+    // Read data (Messages from the client)
+    private BufferedReader bufferedReader;
+    // Send data (Messages to clients from other clients)
+    private BufferedWriter bufferedWriter;
+    private String clientUserName;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUserName = bufferedReader.readLine();
+            clientHandlers.add(this); // Adds client to the clientHandlers list.
+            broadcastMessage("SERVER: " + clientUserName + " has entered the chat!");
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    @Override
+    public void run() {
+        // Everything in this method is run on a separate thread
+        String messageFromClient;
+
+        while (socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+
+    public void broadcastMessage(String messageToSend) {
+        for (ClientHandler clientHandler : clientHandlers) {
+            try {
+                if (!clientHandler.clientUserName.equals(clientUserName)) {
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine();
+                    // A bufferedWriter will not be sent down its upwards stream unless its full.
+                    // And the messages we are sending wont be big enough.
+                    // So we need to flush it manually.
+                    clientHandler.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+    public void removeCLientHandler() {
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: " + clientUserName + " has left the chat!");
+    }
+
+    // With streams you only need to close the outer wrapper as the underlying
+    // streams are closed when you close the wrapper.
+    // Closing a socket will also close the sockets input/output stream.
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        removeCLientHandler();
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
